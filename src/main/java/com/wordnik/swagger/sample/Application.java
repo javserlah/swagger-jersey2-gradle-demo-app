bc6209900220c97a5f115766a998c68553ff6667
Zenedith <github@zenedith.mailgun.org>
2014-02-13 12:00:31 +0100

    Updated grdle files and scopes

--- src/main/java/com/wordnik/swagger/sample/Application.java
+++ src/main/java/com/wordnik/swagger/sample/Application.java
@@ -3,6 +3,7 @@ package com.wordnik.swagger.sample;
 import com.wordnik.swagger.jersey.listing.ApiListingResourceJSON;
 import com.wordnik.swagger.jersey.listing.JerseyApiDeclarationProvider;
 import com.wordnik.swagger.jersey.listing.JerseyResourceListingProvider;
+import org.glassfish.jersey.filter.*;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class Application extends ResourceConfig {
@@ -10,6 +11,7 @@ public class Application extends ResourceConfig {
   public Application() {
     packages("com.wordnik.swagger.sample.resource").
     packages("com.wordnik.swagger.sample.util").
+    register(LoggingFilter.class).
     register(ApiListingResourceJSON.class).
     register(JerseyApiDeclarationProvider.class).
     register(JerseyResourceListingProvider.class);
